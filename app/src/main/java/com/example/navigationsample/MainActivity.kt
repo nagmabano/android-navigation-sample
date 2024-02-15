9befcf11c250684e8f2cbad6b74b769d8f034fad
Nagma Bano <nagma.bano@sap.com>
2024-02-15 23:46:16 +0530

    First screen

--- app/src/main/java/com/example/navigationsample/MainActivity.kt
+++ app/src/main/java/com/example/navigationsample/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.navigationsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.navigationsample.ui.theme.NavigationSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +36,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FirstScreen() {
+    val name = remember {
+        mutableStateOf("")
+    }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "This is the first screen", fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = name.value, onValueChange = {
+            name.value = it
+        })
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Go to second screen")
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    NavigationSampleTheme {
-        Greeting("Android")
-    }
+@Preview(showBackground = true)
+fun FirstPreview() {
+    FirstScreen()
 }
